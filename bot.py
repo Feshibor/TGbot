b0d6d0caa29b0a93d75dea502150a653cbadc417
Feshibor <123161809+Feshibor@users.noreply.github.com>
2025-03-31 21:31:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,43 +6,73 @@ bot = telebot.TeleBot('7841440458:AAFwMEasA_AkaEy7CwjdKlEadLHiBBloPyI') # под
 conn = sqlite3.connect('C:/bot/pictures.db', check_same_thread=False) # подключение к бд
 cursor = conn.cursor()
 
-TableFed = cursor.execute("""SELECT * from feedback""").fetchall() # вся таблица отзывов
-TablePicFed = cursor.execute("""SELECT * from picfeedback""").fetchall() # вся таблица отзывов на картины
-TableArt = cursor.execute("""SELECT * from artists""").fetchall()  # вся таблица художников
-TablePic = cursor.execute("""SELECT * from pictures""").fetchall() # вся таблица картин
+def DataBase(): #обновление данных взятых из бд
+    global TableFed,TablePicFed,TableArt,TablePic,All_artists,All_pictures,All_discription,All_ID
+    TableFed = cursor.execute("""SELECT * from feedback""").fetchall() # вся таблица отзывов
+    TablePicFed = cursor.execute("""SELECT * from picfeedback""").fetchall() # вся таблица отзывов на картины
+    TableArt = cursor.execute("""SELECT * from artists""").fetchall()  # вся таблица художников
+    TablePic = cursor.execute("""SELECT * from pictures""").fetchall() # вся таблица картин
 
-All_artists = []  # список всех художников
-for i in TableArt:
-    All_artists.append(i[1])
-
-
-All_pictures = {}  # словарь имя картины : путь к ней на пк
-for i in TablePic:
-    All_pictures[i[2]] = i[1].replace("\\", "/").replace('"', '')
-
-All_discription = {}  # словарь имя картины : ее описание
-for i in TablePic:
-    All_discription[i[2]] = i[3]
-
-All_ID = {}  # словарь имя картины : ID
-for i in TablePic:
-    All_ID[i[2]] = i[0]
+    All_artists = []  # список всех художников
+    for i in TableArt:
+        All_artists.append(i[1])
 
 
+    All_pictures = {}  # словарь имя картины : путь к ней на пк
+    for i in TablePic:
+        All_pictures[i[2]] = i[1].replace("\\", "/").replace('"', '')
+
+    All_discription = {}  # словарь имя картины : ее описание
+    for i in TablePic:
+        All_discription[i[2]] = i[3]
+
+    All_ID = {}  # словарь имя картины : ID
+    for i in TablePic:
+        All_ID[i[2]] = i[0]
+DataBase()
+@bot.message_handler(func=lambda message: message.text == 'Не Администратор')  # выход из админа
 @bot.message_handler(commands=['start']) # функция на старт
-def start_message(message): 
+def start_message(message):
+    DataBase()
+    global Admin
+    Admin=0
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button1 = types.KeyboardButton("Каталог художников")
     button2 = types.KeyboardButton("Каталог картин")
     button3 = types.KeyboardButton("Посмотреть отзывы о выставке")
     button4 = types.KeyboardButton("Написать отзыв о выставке")
+    button5 = types.KeyboardButton("Администратор")
     keyboard.add(button1, button2)
     keyboard.add(button3, button4)
+    keyboard.add(button5)
     bot.send_message(message.from_user.id, "ОХАЕ", reply_markup=keyboard)
+
+@bot.message_handler(func=lambda message: message.text == 'Администратор')  #
+def Admin(message):
+    DataBase()
+    global Admin
+    Admin=1
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = types.KeyboardButton("Каталог художников")
+    button2 = types.KeyboardButton("Каталог картин")
+    button3 = types.KeyboardButton("Добавить картину")
+    button4 = types.KeyboardButton("Добавить художника")
+    button7 = types.KeyboardButton("Не Администратор")
+    keyboard.add(button1, button2)
+    keyboard.add(button3, button4)
+    keyboard.add(button7)
+    bot.send_message(message.from_user.id, "ОХАЕ", reply_markup=keyboard)
+
+@bot.message_handler(func=lambda message: message.text == 'Добавить художника')  # выводит список всех картин
+def ALL_Artists_list(message):
+    DataBase()
+
+
 
 
 @bot.callback_query_handler(func=lambda call: call.data[:5] == 'desa1')  # описание картин
 def Discription_Pic(call):
+    DataBase()
     message = call.message
     chat_id = message.chat.id
     bot.send_message(message.chat.id, All_discription[(call.data.split("desa1")[1])])
@@ -50,6 +80,7 @@ def Discription_Pic(call):
 
 @bot.callback_query_handler(func=lambda call: call.data[:5] == 'desa0')  # описание художников
 def Discription_Art(call):
+    DataBase()
     message = call.message
     chat_id = message.chat.id
     bot.send_message(message.chat.id, TableArt[All_artists.index(call.data.split("desa0")[1])][2])
@@ -57,6 +88,7 @@ def Discription_Art(call):
 
 @bot.callback_query_handler(func=lambda call: call.data[:5] == 'sfeed')  # выводит все отзывы о картине
 def Seefeed(call):
+    DataBase()
     message = call.message
     chat_id = message.chat.id
     b = All_ID[(call.data.split("sfeed")[1])]
@@ -102,15 +134,70 @@ def Write_feedback(message):  # я хз как это работает но я �
 
 @bot.message_handler(func=lambda message: message.text == 'Каталог художников')  # выводит список всех художников
 def ALL_Pictures_list(message):
-    for i in All_artists:
-        keyboard1 = telebot.types.InlineKeyboardMarkup()
-        button_save = telebot.types.InlineKeyboardButton(text="Описание", callback_data=f'desa0{i}')
-        keyboard1.add(button_save)
-        bot.send_message(message.from_user.id, i, reply_markup=keyboard1)
+    DataBase()
+    if Admin==0:
+        for i in All_artists:
+            keyboard1 = telebot.types.InlineKeyboardMarkup()
+            button_save = telebot.types.InlineKeyboardButton(text="Описание", callback_data=f'desa0{i}')
+            keyboard1.add(button_save)
+            bot.send_message(message.from_user.id, i, reply_markup=keyboard1)
+    elif Admin==1:
+        for i in All_artists:
+            keyboard1 = telebot.types.InlineKeyboardMarkup()
+            button1 = telebot.types.InlineKeyboardButton(text="Изменить Описание", callback_data=f'redao{i}')
+            button3 = telebot.types.InlineKeyboardButton(text="Изменить Имя", callback_data=f'redan{i}')
+            button2 = telebot.types.InlineKeyboardButton(text="Удалить", callback_data=f'delet{i}')
+            keyboard1.add(button1,button3)
+            keyboard1.add(button2)
+            bot.send_message(message.from_user.id, i, reply_markup=keyboard1)
+
+
+
+
+
+@bot.callback_query_handler(func=lambda call: call.data[:5] == 'delet')  # удалить все о выбранной картине
+def Delete_Artist(call):
+    Name_Artists_Delete = call.data.split("delet")[1]
+    cursor.execute(f'DELETE FROM artists WHERE creator_name = "{Name_Artists_Delete}"')
+    conn.commit()
+
+@bot.callback_query_handler(func=lambda call: call.data[:5] == 'redan')  # изменить описание картины
+def Redact_Artists(call):
+    global Name_Artists_Readct
+    Name_Artists_Readct = call.data.split("redan")[1]
+    message = call.message
+    chat_id = message.chat.id
+    bot.send_message(message.chat.id,f'Имя в данный момент: {TableArt[All_artists.index(call.data.split("redan")[1])][1]}')
+    mesg = bot.send_message(message.chat.id,f'Напишите на что его заменить')
+    bot.register_next_step_handler(mesg, Redact_Artists_Update1)
+
+def Redact_Artists_Update1(message): # изменить описание картины после получения сообщения
+    cursor.execute(f'UPDATE artists SET creator_name = "{message.text}" where creator_name = "{Name_Artists_Readct}" ')
+    conn.commit()
+    bot.send_message(message.chat.id, f'Имя сохранено')
+
+
+@bot.callback_query_handler(func=lambda call: call.data[:5] == 'redao')  # изменить описание картины
+def Redact_Artists(call):
+    global Name_Artists_Readct
+    Name_Artists_Readct = call.data.split("redao")[1]
+    message = call.message
+    chat_id = message.chat.id
+    bot.send_message(message.chat.id,f'Описание в данный момент: {TableArt[All_artists.index(call.data.split("redao")[1])][2]}')
+    mesg = bot.send_message(message.chat.id,f'Напишите на что его заменить')
+    bot.register_next_step_handler(mesg, Redact_Artists_Update)
+
+def Redact_Artists_Update(message): # изменить описание картины после получения сообщения
+    cursor.execute(f'UPDATE artists SET description = "{message.text}" where creator_name = "{Name_Artists_Readct}" ')
+    conn.commit()
+    bot.send_message(message.chat.id, f'Ваш отзыв сохранен')
+
+
 
 
 @bot.message_handler(func=lambda message: message.text == 'Каталог картин')  # выводит список всех картин
 def ALL_Artists_list(message):
+    DataBase()
     for i, v in All_pictures.items():
         keyboard1 = telebot.types.InlineKeyboardMarkup()
         button_save = telebot.types.InlineKeyboardButton(text="Описание", callback_data=f'desa1{i}')
@@ -123,6 +210,7 @@ def ALL_Artists_list(message):
 
 @bot.message_handler(func=lambda message: message.text == 'Посмотреть отзывы о выставке')  # вывод отзывов (5 с конца)
 def See_5_feedback(message):
+    DataBase()
     TableFed = cursor.execute("""SELECT * from feedback""").fetchall()
     for i in range(len(TableFed)):
         bot.send_message(message.from_user.id,f'{i+1}) {TableFed[-1 * (i + 1)][0]}')
@@ -138,8 +226,8 @@ def save_feedback(message):  # добавление отзыва о всей в�
     bot.send_message(message.from_user.id, "Ваш отзыв сохранён")
 
 
-@bot.message_handler(func=lambda message: message.text == 'Написать отзыв о выставке')  # написать отзыв 
-def write_to_support(message):
+@bot.message_handler(func=lambda message: message.text == 'Написать отзыв о выставке')  # написать отзыв
+def Write_All_Feedback(message):
     bot.send_message(message.from_user.id, "Напишите отзыв")
     bot.register_next_step_handler(message, save_feedback)
 
